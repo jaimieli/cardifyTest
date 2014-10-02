ea665198d515f68620053d0b6268d128de1241a2
Jaimie Li <jaimieli@gmail.com>
2014-10-02 17:56:51 -0400

    formatted date object, currency, etc

--- client/app/displayAngel/displayAngel.controller.js
+++ client/app/displayAngel/displayAngel.controller.js
@@ -2,6 +2,10 @@
 
 angular.module('cardifyTestApp')
   .controller('DisplayangelCtrl', function ($scope, $http) {
+    // format number to have commas
+    var numberWithCommas = function(x){
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
     this.getAngel = function(company) {
       // reset scope variables when searching for a new company
       $scope.userData = null;
@@ -16,8 +20,18 @@ angular.module('cardifyTestApp')
       $http.get('/api/angels/getAngel/' + company).success(function(data){
         console.log('Company Data: ', data);
         $scope.companyData = data;
-      })
-    }
+        // format salary to have commas + $
+        $scope.companyData.jobs.forEach(function(el){
+          el.salary_max_display = '$' + numberWithCommas(el.salary_max);
+          el.salary_min_display = '$' + numberWithCommas(el.salary_min);
+        });
+        // format funding to have commas + $
+        $scope.companyData.crunchbase.properties.total_funding_usd_display = '$' + numberWithCommas($scope.companyData.crunchbase.properties.total_funding_usd);
+        // format date
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        $scope.companyData.crunchbase.properties.founded_on_display = months[$scope.companyData.crunchbase.properties.founded_on_month - 1] + ' ' + $scope.companyData.crunchbase.properties.founded_on_day + ', ' + $scope.companyData.crunchbase.properties.founded_on_year;
+      });
+    };
     this.getEmployee = function(user) {
       console.log('id: ', user)
       $http.get('/api/angels/getUser/' + user).success(function(data){
